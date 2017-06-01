282d5e1bff5260d98ce2e03d945dead1111ff75a
xurongrong <xurongrong0821@gmail.com>
2017-06-01 17:15:20 +0800

    comment bug

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,74 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!, only: [:create, :destroy]
+
+
+
+  # GET /comments
+  # GET /comments.json
+  def index
+    @comments = Comment.all
+  end
+
+  # GET /comments/1
+  # GET /comments/1.json
+  def show
+  end
+
+  # GET /comments/new
+  def new
+    @comment = Comment.new
+  end
+
+  # GET /comments/1/edit
+  def edit
+  end
+
+  # POST /comments
+  # POST /comments.json
+    def create
+      @product = Product.find(params[:product_id])
+      @comment = @product.comments.new(comment_params)
+      @comment.user = current_user
+
+      if @comment.save
+        redirect_to product_path(@product), notice: 'Review was successfully created!'
+      else
+        redirect_to product_path(@product), alert: "Comment can't be blank!"
+      end
+    end
+
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @product = Product.find(params[:product_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to product_path(@product), alert: "You have deleted the comment successfully"
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:product_id, :body, :user_id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522062941) do
+ActiveRecord::Schema.define(version: 20170601083150) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20170522062941) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "product_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -61,9 +71,10 @@ ActiveRecord::Schema.define(version: 20170522062941) do
     t.text     "description"
     t.integer  "quantity"
     t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "image"
+    t.string   "product_name"
     t.integer  "category_id"
   end
 
