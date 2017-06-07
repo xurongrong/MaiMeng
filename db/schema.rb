a0bb56aa43cba0a110741d39054233807adfd42e
xurongrong <xurongrong0821@gmail.com>
2017-06-07 16:20:52 +0800

    add product detail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601083150) do
+ActiveRecord::Schema.define(version: 20170607081515) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -71,10 +71,12 @@ ActiveRecord::Schema.define(version: 20170601083150) do
     t.text     "description"
     t.integer  "quantity"
     t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "image"
+    t.string   "product_name"
     t.integer  "category_id"
+    t.text     "detail"
   end
 
   create_table "users", force: :cascade do |t|
