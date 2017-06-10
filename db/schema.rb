5aa90d9e6dde23ed745308aa41577067cf948c55
lihantao <htl00001111@163.com>
2017-06-10 23:15:57 +0800

    add photos to product and renew seed.rb/admin can delete product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607081515) do
+ActiveRecord::Schema.define(version: 20170610074408) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -57,6 +57,13 @@ ActiveRecord::Schema.define(version: 20170607081515) do
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.integer  "product_id"
+    t.string   "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "product_lists", force: :cascade do |t|
     t.integer  "order_id"
     t.string   "product_name"
@@ -71,10 +78,9 @@ ActiveRecord::Schema.define(version: 20170607081515) do
     t.text     "description"
     t.integer  "quantity"
     t.integer  "price"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "image"
-    t.string   "product_name"
     t.integer  "category_id"
     t.text     "detail"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,64 +21,78 @@ end
 
 #添加表情分类
 Category.create!( :name => "免费表情" )
-Category.create!( :name => "个人定制" )
-Category.create!( :name => "情侣定制" )
-Category.create!( :name => "动态表情" )
+# Category.create!( :name => "个人定制" )
+# Category.create!( :name => "情侣定制" )
+# Category.create!( :name => "动态表情" )
 puts "添加商品分类。"
 
-#添加表情数量
-#前四个表情展示到首页一组
-Product.create!(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201607/15/217d365b19.gif"))
-Product.create!(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201603/23/32cd54c04c.gif"))
-Product.create!(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201605/17/8413712bd4.gif"))
-Product.create!(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201512/23/fa1bcfe7c4.gif"))
-#免费表情 宋仲基二组
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201607/15/486b683703.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201607/15/6180544eb5.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201607/15/3d67cbde48.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201607/15/0975ba7d1c.gif"))
-#免费表情 林更新三组
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201603/23/55442410a5.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201603/23/9d33109bfd.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201603/23/5a4ce9c782.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201603/23/89dd783256.gif"))
-#免费表情 屌丝男士四组
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201605/17/579e7aec92.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201605/17/11ad1ff9d3.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201605/17/c443a6c8c6.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201605/17/c337b88b0b.gif"))
-#免费表情 杨颖五组
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201509/30/84b9a8986f.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201509/30/4a4d8cab2b.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201509/30/a88c4ef776.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201509/30/0a0ec9479d.gif"))
-#免费表情 跑男六组
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201512/23/db4a0e50bd.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201512/23/7fef66d498.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201512/23/fa1bcfe7c4.gif"))
-Product.create!(title:"免费表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
-image: open("http://img.wxcha.com/file/201512/23/6f70b521dd.gif"))
-puts "添加六组表情包。"
+#添加多图表情
+#前四个表情包展示到首页
+# Product.create!(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
+# image: open("http://img.wxcha.com/file/201607/15/217d365b19.gif"))
+# Product.create!(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
+# image: open("http://img.wxcha.com/file/201603/23/32cd54c04c.gif"))
+# Product.create!(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
+# image: open("http://img.wxcha.com/file/201605/17/8413712bd4.gif"))
+# Product.create!(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1,
+# image: open("http://img.wxcha.com/file/201512/23/fa1bcfe7c4.gif"))
+p1 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:1,avatar: open("http://img.wxcha.com/file/201607/15/217d365b19.gif"))
+Photo.create!(product_id:1,avatar: open("http://img.wxcha.com/file/201607/15/486b683703.gif"))
+Photo.create!(product_id:1,avatar: open("http://img.wxcha.com/file/201607/15/0975ba7d1c.gif"))
+p2 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:2,avatar: open("http://img.wxcha.com/file/201603/23/32cd54c04c.gif"))
+Photo.create!(product_id:2,avatar: open("http://img.wxcha.com/file/201603/23/9d33109bfd.gif"))
+Photo.create!(product_id:2,avatar: open("http://img.wxcha.com/file/201603/23/89dd783256.gif"))
+p3 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:3,avatar: open("http://img.wxcha.com/file/201605/17/8413712bd4.gif"))
+Photo.create!(product_id:3,avatar: open("http://img.wxcha.com/file/201605/17/c443a6c8c6.gif"))
+Photo.create!(product_id:3,avatar: open("http://img.wxcha.com/file/201605/17/c337b88b0b.gif"))
+p4 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:4,avatar: open("http://img.wxcha.com/file/201512/23/fa1bcfe7c4.gif"))
+Photo.create!(product_id:4,avatar: open("http://img.wxcha.com/file/201512/23/fa1bcfe7c4.gif"))
+Photo.create!(product_id:4,avatar: open("http://img.wxcha.com/file/201512/23/6f70b521dd.gif"))
+#免费表情 宋仲基林更新4组
+p5 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:5,avatar: open("http://img.wxcha.com/file/201607/15/486b683703.gif"))
+Photo.create!(product_id:5,avatar: open("http://img.wxcha.com/file/201607/15/6180544eb5.gif"))
+Photo.create!(product_id:5,avatar: open("http://img.wxcha.com/file/201607/15/3d67cbde48.gif"))
+p6 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:6,avatar: open("http://img.wxcha.com/file/201607/15/0975ba7d1c.gif"))
+Photo.create!(product_id:6,avatar: open("http://img.wxcha.com/file/201607/15/0975ba7d1c.gif"))
+Photo.create!(product_id:6,avatar: open("http://img.wxcha.com/file/201607/15/0975ba7d1c.gif"))
+p7 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:7,avatar: open("http://img.wxcha.com/file/201603/23/55442410a5.gif"))
+Photo.create!(product_id:7,avatar: open("http://img.wxcha.com/file/201603/23/9d33109bfd.gif"))
+Photo.create!(product_id:7,avatar: open("http://img.wxcha.com/file/201603/23/5a4ce9c782.gif"))
+p8 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:8,avatar: open("http://img.wxcha.com/file/201603/23/89dd783256.gif"))
+Photo.create!(product_id:8,avatar: open("http://img.wxcha.com/file/201603/23/89dd783256.gif"))
+Photo.create!(product_id:8,avatar: open("http://img.wxcha.com/file/201603/23/89dd783256.gif"))
+#免费表情 屌丝男士杨颖4组
+p9 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:9,avatar: open("http://img.wxcha.com/file/201605/17/579e7aec92.gif"))
+Photo.create!(product_id:9,avatar: open("http://img.wxcha.com/file/201605/17/11ad1ff9d3.gif"))
+Photo.create!(product_id:9,avatar: open("http://img.wxcha.com/file/201605/17/c443a6c8c6.gif"))
+p10 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:10,avatar: open("http://img.wxcha.com/file/201605/17/c337b88b0b.gif"))
+Photo.create!(product_id:10,avatar: open("http://img.wxcha.com/file/201605/17/c337b88b0b.gif"))
+Photo.create!(product_id:10,avatar: open("http://img.wxcha.com/file/201605/17/c337b88b0b.gif"))
+p11 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:11,avatar: open("http://img.wxcha.com/file/201509/30/84b9a8986f.gif"))
+Photo.create!(product_id:11,avatar: open("http://img.wxcha.com/file/201509/30/4a4d8cab2b.gif"))
+Photo.create!(product_id:11,avatar: open("http://img.wxcha.com/file/201509/30/a88c4ef776.gif"))
+p12 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:12,avatar: open("http://img.wxcha.com/file/201509/30/0a0ec9479d.gif"))
+Photo.create!(product_id:12,avatar: open("http://img.wxcha.com/file/201509/30/0a0ec9479d.gif"))
+Photo.create!(product_id:12,avatar: open("http://img.wxcha.com/file/201509/30/0a0ec9479d.gif"))
+#免费表情 跑男2组
+p13 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:13,avatar: open("http://img.wxcha.com/file/201512/23/db4a0e50bd.gif"))
+Photo.create!(product_id:13,avatar: open("http://img.wxcha.com/file/201512/23/7fef66d498.gif"))
+Photo.create!(product_id:13,avatar: open("http://img.wxcha.com/file/201512/23/fa1bcfe7c4.gif"))
+p14 = Product.create(title:"最热表情", description: "免费表情", quantity:9999, price:0, category_id: 1)
+Photo.create!(product_id:14,avatar: open("http://img.wxcha.com/file/201512/23/6f70b521dd.gif"))
+Photo.create!(product_id:14,avatar: open("http://img.wxcha.com/file/201512/23/6f70b521dd.gif"))
+Photo.create!(product_id:14,avatar: open("http://img.wxcha.com/file/201512/23/6f70b521dd.gif"))
+puts "添加14组表情包。"
